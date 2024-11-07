9f2a780ddc59255e190ca05d8ecc858f870327c5
vigbalar <vigbalar@gmail.com>
2024-11-08 00:45:55 +0530

    second commit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,6 +2,7 @@ import random
 
 user_wins = 0
 computer_wins = 0
+tie = 0
 
 options = ["rock", "paper", "scissors"]
 
@@ -30,11 +31,16 @@ while True:
         print("You Won!!")
         user_wins += 1
         
+    elif (user_input == "scissors" and computer_pick == "scissors") or (user_input == "paper" and computer_pick == "paper") or (user_input == "rock" and computer_pick == "rock"):
+        print("TIE!!")
+        tie += 1
+        
     else:
         print("You Lost!!")
         computer_wins += 1
         
 print("You won", user_wins, "times.")
 print("Computer won", computer_wins, "times.")
+print(" Tie", tie, "times.")
 print("Goodbye!")
         
